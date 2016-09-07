ef932d9e5be4d3a0237dfed192d43af265b46307
bocahapi <wichaksono.w@gmail.com>
2016-09-08 06:46:18 +0700

    theme semi toko online

---
+++ footer.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains footer content and the closing of the #main and #page div elements.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+?>
+	</div><!-- #main .wrapper -->
+	<footer id="colophon" role="contentinfo">
+		<div class="site-info">
+			<?php do_action( 'semi-toko-online_credits' ); ?>
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'semi-toko-online' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'semi-toko-online' ); ?>"><?php printf( __( 'Proudly powered by %s', 'semi-toko-online' ), 'WordPress' ); ?></a>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+</body>
+</html>
